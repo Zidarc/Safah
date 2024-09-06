17f746f76b1139a446d91113e51f4b472ee5512b
Ali Hussain <ali.hussaino71205o@gmail.com>
2024-09-06 22:15:12 +0500

    tessst

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ const fileInput = document.getElementById("fileUpload");
 const dragDropArea = document.getElementById("dragDropArea");
 const output = document.getElementById("output");
 let imageList = [];
+
 const imagekit = new ImageKit({
     publicKey: "public_XT2xiMLSDVPDCVgXODwralEaBso=",
     urlEndpoint: "https://ik.imagekit.io/hmlgaiv6o",
@@ -73,30 +74,6 @@ dragDropArea.addEventListener("drop", (e) => {
     });
 });
 
-const createFolder = async (folderPath) => {
-    try {
-        const folderName = folderPath.split('/').pop();
-        const parentFolderPath = folderPath.substring(0, folderPath.lastIndexOf('/')) || '/';
-        
-        const response = await new Promise((resolve, reject) => {
-            imagekit.createFolder({
-                folderName: folderName,
-                parentFolderPath: parentFolderPath
-            }, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-        
-        console.log('Folder creation result:', response);
-    } catch (error) {
-        throw new Error('Failed to create folder: ' + error.message);
-    }
-};
-
 document.addEventListener("DOMContentLoaded", () => {
     const submitBtn = document.getElementById("submitBtn");
 
@@ -112,11 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
 
-        const folderPath = `/Home/${email}`;
-
         try {
-            await createFolder(folderPath); // Ensure the folder is created before uploading files
-
             for (let i = 0; i < imageList.length; i++) {
                 const file = imageList[i];
                 const fileName = `${name}_${file.name}`;
@@ -130,7 +103,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 const uploadResponse = await imagekit.upload({
                     file: file,
                     fileName: fileName,
-                    folder: folderPath,
                     token: authData.token,
                     signature: authData.signature,
                     expire: authData.expire,
@@ -165,5 +137,6 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error('Error:', error.message);
             alert('An error occurred while uploading the images or submitting the form.');
         }
+        
     });
 });
